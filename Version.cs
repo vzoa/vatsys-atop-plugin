20ef4b71d2e3af88b28e0b484326c388341e9f12
Maxim Salikhov <msalihov@gmail.com>
2024-04-14 00:19:36 -0400

    Change disconnected indication to ADSC flag (#4)
    * Change disconnected indication to ADSC flag
    
    * Bump version to 2403.1.1

--- Version.cs
+++ Version.cs
@@ -5,7 +5,7 @@ public static class Version
     private const string VersionPrefix = "Version ";
     private const int AiracVersion = 2403;
     private const int MajorVersion = 1;
-    private const int MinorVersion = 0;
+    private const int MinorVersion = 1;
 
     public static string GetVersionString()
     {
