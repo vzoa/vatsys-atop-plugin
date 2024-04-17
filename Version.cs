2794ada1a8d94d1e2ab66399d031d1d7f066793a
Maxim Salikhov <msalihov@gmail.com>
2024-04-17 19:53:16 -0400

    Bump version to 2024.0.0 release

--- Version.cs
+++ Version.cs
@@ -3,8 +3,8 @@ namespace AtopPlugin;
 public static class Version
 {
     private const string VersionPrefix = "Version ";
-    private const int AiracVersion = 2403;
-    private const int MajorVersion = 3;
+    private const int AiracVersion = 2404;
+    private const int MajorVersion = 0;
     private const int MinorVersion = 0;
 
     public static string GetVersionString()
